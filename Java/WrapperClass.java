5fe315fa8fca2aa8085785dd41020a22ab5490bc
Younosnaseri <115519969+Younosnaseri@users.noreply.github.com>
2022-11-29 01:29:35 +0430

    Add files via upload

---
+++ Java/WrapperClass.java
@@ -0,0 +1,11 @@
+package wrapperclass;
+public class WrapperClass {
+    public static void main(String[] args) {
+    Integer age=12;
+    System.out.println(age);
+     Double width=100.0;
+     System.out.println(width);
+    
+    }
+    
+}
